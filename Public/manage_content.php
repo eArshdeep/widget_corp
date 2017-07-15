4fa012f68eee5a906584cd8d60aa8b372b66674a
Arshdeep Singh Padda <aspadda@icloud.com>
2017-07-15 13:39:47 -0700

    Code Refactor for (manage_content.php)
    (manage_content.php) now includes modular versions of page and subject overview which are in (includes/layout). Thus, (manage_content.php) is a lot cleaner to view.

--- Public/manage_content.php
+++ Public/manage_content.php
@@ -28,75 +28,12 @@
       <section class="col s12 m10">
         <div class="container">
           <?php
-            // SUBJECT OVERVIEW
-            if(isset($current_subject)) { /* close php tags for raw html */ ?>
-
-              <!-- title -->
-              <h2>Subject Overview</h2>
-              <!-- menu name -->
-              <p class='bold'>Menu Name:
-                <span class="normal"><?php echo htmlentities($current_subject["menu_name"]); ?></span>
-              </p>
-              <!-- position -->
-              <p> <span class='bold'>Position:</span> <?php echo $current_subject["position"]; ?> <p>
-              <!-- visibility -->
-              <p> <span class='bold'>Status:</span> <?php echo $current_subject["visible"] == 1 ? "Shown" : "Hidden"; ?> <p>
-              <!-- break after subject properties section -->
-              <br>
-              <!-- edit button -->
-              <a href="edit_subject.php?subject=<?php echo urlencode($current_subject["id"]); ?>" class="waves-effect waves-light btn">
-                <i class='material-icons left'>mode_edit</i> Edit </a>
-              <!-- delete button -->
-              <a class="waves-effect waves-light btn" href="#delete_modal">
-                <i class='material-icons left'>delete</i> Delete </a>
-              <?php /*Include structure for deletion modal*/ include '../includes/layout/delete_modal.php'; ?>
-
-              <?php /* Show subject's child pages if any*/ include '../includes/layout/subject_child_pages.php'; ?>
-
-          <?php  } /* end of if(isset($current_subject)) */
-            elseif (isset($current_page)){ /* end php tags for raw html */ ?>
-              <?php
-                // get parent subject
-                $parent_subject = find_subject_by_id($current_page["subject_id"]);
-              ?>
-              <!-- Page Overview -->
-              <!-- Title -->
-              <h2>Page Overview</h2>
-
-              <!-- Menu Name -->
-              <p class="bold">Page Name:
-                <span class="normal"><?php echo htmlentities($current_page["menu_name"]); ?></span>
-              </p>
-
-              <!-- Subject Name -->
-              <p class="bold">Parent Subject:
-                <a href="manage_content.php?subject=<?php echo urlencode($parent_subject["id"]); ?>" class="normal link_underline"><?php echo htmlentities($parent_subject["menu_name"]); ?></a>
-              </p>
-
-              <!-- position -->
-              <p> <span class='bold'>Page Position:</span> <?php echo $current_page["position"]; ?> <p>
-
-              <!-- visibility -->
-              <p> <span class='bold'>Status:</span> <?php echo $current_page["visible"] == 1 ? "Shown" : "Hidden"; ?> <p>
-
-              <!-- Content -->
-              <div class="input-field margin-large-adder">
-                <textarea disabled id="content" class="materialize-textarea"> <?php echo htmlentities($current_page["content"]); ?> </textarea>
-                <label for="content">Content</label>
-              </div>
-
-              <!-- Links for delete and edit-->
-              <!-- edit button -->
-              <a href="edit_page.php?subject=<?php echo urlencode($current_page["id"]); ?>" class="waves-effect waves-light btn">
-                <i class='material-icons left'>mode_edit</i> Edit </a>
-
-              <!-- delete button -->
-              <a class="waves-effect waves-light btn" href="#delete_modal">
-                <i class='material-icons left'>delete</i> Delete </a>
-
-              <?php /*Include structure for deletion modal*/ include '../includes/layout/delete_modal.php'; ?>
-
-          <?php } // END elseif (isset($current_page))
+            if(isset($current_subject)) {
+              include '../includes/layout/subject_overview.php';
+            }
+            elseif (isset($current_page)){
+              include '../includes/layout/page_overview.php';
+            }
             else {
               echo "<p>Please select a page or subject to manage or modify.</p>";
             }
---
+++ includes/layout/subject_overview.php
@@ -0,0 +1,21 @@
+<!-- title -->
+<h2>Subject Overview</h2>
+<!-- menu name -->
+<p class='bold'>Menu Name:
+  <span class="normal"><?php echo htmlentities($current_subject["menu_name"]); ?></span>
+</p>
+<!-- position -->
+<p> <span class='bold'>Position:</span> <?php echo $current_subject["position"]; ?> <p>
+<!-- visibility -->
+<p> <span class='bold'>Status:</span> <?php echo $current_subject["visible"] == 1 ? "Shown" : "Hidden"; ?> <p>
+<!-- break after subject properties section -->
+<br>
+<!-- edit button -->
+<a href="edit_subject.php?subject=<?php echo urlencode($current_subject["id"]); ?>" class="waves-effect waves-light btn">
+  <i class='material-icons left'>mode_edit</i> Edit </a>
+<!-- delete button -->
+<a class="waves-effect waves-light btn" href="#delete_modal">
+  <i class='material-icons left'>delete</i> Delete </a>
+<?php /*Include structure for deletion modal*/ include '../includes/layout/delete_modal.php'; ?>
+
+<?php /* Show subject's child pages if any*/ include '../includes/layout/subject_child_pages.php'; ?>
