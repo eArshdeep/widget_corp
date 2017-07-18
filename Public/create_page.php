27cdb7d727de406b0c9e036479842b51ccec3dc1
Arshdeep Padda <aspadda@icloud.com>
2017-07-18 13:19:01 -0700

    Added ability to create new pages

---
+++ Public/create_page.php
@@ -0,0 +1,76 @@
+<?php require_once '../includes/session.php'; ?>
+<?php require_once '../includes/db_connection.php'; ?>
+<?php require_once '../includes/functions.php'; ?>
+<?php require_once '../includes/validation.php'; ?>
+
+<?php
+  if(isset($_POST["submit"])){
+    // handle values
+    $menu_name = $_POST["menu_name"];
+    $position = (int) $_POST["position"];
+    $subject_id = (int) $_POST["subject_id"];
+    if(isset($_POST["visible"])){
+      $visible = (int) $_POST["visible"];
+    }
+    $content = $_POST["content"];
+
+    // escape
+    $menu_name = mysqli_real_escape_string($db, $menu_name);
+    $content = mysqli_real_escape_string($db, $content);
+
+    // validate form values
+    $required_fields = array("menu_name", "position", "visible", "subject_id", "content");
+    validate_presences($required_fields);
+
+    $fields_with_length_limit = array("menu_name" => 30);
+    validate_max_lengths($fields_with_length_limit);
+
+    if(!empty($errors)){
+      // put form validation errors in session
+      $_SESSION["errors"] = $errors;
+
+      // put values entered by user into session to repopulate form
+      if(has_presence($menu_name)){
+        $_SESSION["repop_menu_name"] = $menu_name;
+      }
+
+      if(has_presence($position) && $position !== 0){
+        $_SESSION["repop_position"] = $position;
+      }
+
+      if(has_presence($visible)){
+        $_SESSION["repop_visible"] = $visible;
+      }
+
+      if(has_presence($content)){
+        $_SESSION["repop_content"] = $content;
+      }
+
+      // redirect user to add page form
+      redirect_to("new_page.php?subject={$subject_id}");
+    }
+
+    // build query
+    $query = "INSERT INTO pages ( menu_name, subject_id, position, visible, content) VALUES ( '{$menu_name}', {$subject_id}, {$position}, {$visible}, '{$content}');";
+    $result = mysqli_query($db, $query);
+
+    if($result){
+      // if creation is successful
+      $_SESSION["message"] = "Page was added successfully. Cheers :)";
+      redirect_to("manage_content.php?subject={$subject_id}");
+    } else {
+      // if creation is NOT successful
+      $_SESSION["message"] = "Unable to add page. Bonkers!";
+      redirect_to("manage_content.php?subject={$subject_id}");
+    }
+
+  } else {
+    // if trying to access this page via a url, and not a post request, redirect to homepage
+    redirect_to("index.php");
+  }
+?>
+
+<?php
+  if(isset($db)){mysqli_close($db);}
+  if(isset($result)){mysqli_free_result($result);}
+?>
--- Public/new_page.php
+++ Public/new_page.php
@@ -11,6 +11,13 @@
   <?php
     generate_header();
     $errors = grab_errors();
+    // get the subject for which we are trying to add the page
+    $parent_subject = find_subject_by_id($_GET["subject"]);
+    // redirect if subject not found
+    if(!isset($parent_subject)){
+      $_SESSION["message"] = "Couldn\'t add a page for that subject, sorry.";
+      redirect_to("manage_content.php");
+    }
   ?>
 
   <main>
@@ -24,21 +31,29 @@
       <section class="col s12 m10">
         <div class="container">
           <div class="divider hide-on-large-only"></div>
-          <h2>Create New Subject</h2>
-          <form action="create_subject.php" method="post">
+          <!-- Title -->
+          <h2>Add New Page for
+            <span class="light-weight">
+              <?php echo $parent_subject["menu_name"]; ?>
+            </span>
+          </h2>
+          <!-- Form -->
+          <form action="create_page.php" method="post">
+
             <!-- Input: menu name -->
             <div class="input-field">
-              <input placeholder="Menu name" id="menu_name" name="menu_name" type="text" <?php repopulate_menu_name("repop_menu_name"); ?>>
-              <label for="menu_name" <?php if(isset($errors["menu_name"])){echo "class='red-text'";} ?>>Menu Name</label>
+              <input placeholder="Page Name" id="menu_name" name="menu_name" type="text" <?php repopulate_menu_name("repop_menu_name"); ?>>
+              <label for="menu_name" <?php if(isset($errors["menu_name"])){echo "class='red-text'";} ?>>Page Name</label>
             </div>
+
             <!-- Input: Position -->
             <div class="input-field">
               <select name="position">
                 <option value="" disabled <?php if(!isset($_SESSION["repop_position"])){echo "selected";} ?> >Select Position</option>
                   <?php
-                  $subject_set = find_all_subjects();
-                  $subject_count = mysqli_num_rows($subject_set);
-                  for($count=1; $count <=$subject_count+1; $count++){
+                  $page_set = find_pages_for_subject($parent_subject["id"]);
+                  $page_count = mysqli_num_rows($page_set);
+                  for($count=1; $count <=$page_count+1; $count++){
                     $output = "<option ";
                     if(isset($_SESSION["repop_position"]) && $_SESSION["repop_position"] === $count){
                       $output .= "selected ";
@@ -52,20 +67,35 @@
                   }
                   ?>
               </select>
-              <label <?php if(isset($errors["position"])){echo "class='red-text'";} ?>>Position</label>
+              <label <?php if(isset($errors["position"])){echo "class='red-text'";} ?>>Page Position</label>
             </div>
+
             <!-- Input: Visibility -->
             <p <?php if(isset($errors["visible"])){echo "class='red-text'";} ?>>Visibility</p>
-              <input name="visible" type="radio" id="visible_true" value="1" <?php repopulate_visibility(1); ?> />
-              <label for="visible_true">Visible</label>
-              <span class="side-margin-adder">or</span>
-              <input name="visible" type="radio" id="visible_hidden" value="0"<?php repopulate_visibility(0); ?> />
-              <label for="visible_hidden">Hidden</label>
+            <input name="visible" type="radio" id="visible_true" value="1" <?php repopulate_visibility(1); ?> />
+            <label for="visible_true">Visible</label>
+            <span class="side-margin-adder">or</span>
+            <input name="visible" type="radio" id="visible_hidden" value="0"<?php repopulate_visibility(0); ?> />
+            <label for="visible_hidden">Hidden</label>
+
+            <!-- Content -->
+            <div class="input-field margin-large-adder">
+              <textarea id="content" class="materialize-textarea" name="content"><?php
+                if (isset($_SESSION["repop_content"])) {
+                  echo htmlentities($_SESSION["repop_content"]);
+                  $_SESSION["repop_content"]=null;
+                } ?></textarea>
+              <label for="content" <?php if(isset($errors["content"])){echo "class='red-text'";} ?> >Content</label>
+            </div>
+
+            <!-- Subject ID -->
+            <input type="hidden" name="subject_id" value="<?php echo htmlentities($parent_subject["id"]); ?>" >
 
             <!-- Submit Button -->
             <button style="display:block;" class="btn waves-effect waves-light margin-adder" type="submit" name="submit" value="true">Create</button>
+
           </form>
-          <a href="manage_content.php" class="orange-text">&#8592; Cancel</a>
+          <a href="manage_content.php?subject=<?php echo urlencode($parent_subject["id"]); ?>" class="orange-text">&#8592; Cancel</a>
 
           <section>
             <?php echo_errors($errors); ?>
@@ -87,7 +117,7 @@
   <?php
     // release db resources and handles
     mysqli_close($db);
-    mysqli_free_result($subject_set);
+    mysqli_free_result($page_set);
   ?>
 </body>
 </html>
